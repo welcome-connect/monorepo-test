47f5759df18b9c44fe2d33ae99b1eaadf8e24865
Sebastian Garces <gsgarces1@gmail.com>
2021-05-19 23:37:36 -0400

    context(team): team provider added

--- apps/agents/pages/_app.tsx
+++ apps/agents/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyles, theme } from '../styles'
 import { AuthProvider } from '../contexts/auth/AuthProvider'
+import { TeamProvider } from '../contexts/team/TeamProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
@@ -11,12 +12,14 @@ function MyApp({ Component, pageProps }: AppProps) {
 				<meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
 				<title>Welcome Dispatch</title>
 			</Head>
-			<AuthProvider>
-				<ThemeProvider theme={theme}>
-					<GlobalStyles />
-					<Component {...pageProps} />
-				</ThemeProvider>
-			</AuthProvider>
+			<TeamProvider>
+				<AuthProvider>
+					<ThemeProvider theme={theme}>
+						<GlobalStyles />
+						<Component {...pageProps} />
+					</ThemeProvider>
+				</AuthProvider>
+			</TeamProvider>
 		</>
 	)
 }
